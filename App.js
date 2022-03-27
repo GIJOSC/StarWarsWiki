5a1d2fe84a16cb4be84825f3e8d4e3979d039f00
Gesiel Neves <neves.gesiel@gmail.com>
2022-03-27 18:03:31 -0300

    Aula02

--- App.js
+++ App.js
@@ -1,7 +1,37 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { SplashScreen } from "./src/screens/SplashScreen";
+import 'react-native-gesture-handler'
+import React from 'react'
+import { Home } from './src/screens'
+import AppLoading from 'expo-app-loading'
+import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  SourceSansPro_400Regular,
+  SourceSansPro_700Bold,
+  SourceSansPro_600SemiBold,
+  SourceSansPro_900Black,
+} from '@expo-google-fonts/source-sans-pro'
+import { theme } from './src/styles'
+import { Routes } from './src/routes'
+import { SplashScreen } from './src/screens/SplashScreen'
+import { NavigationContainer } from '@react-navigation/native'
+
+// AsyncStorage.clear()
 
 export default function App() {
-  return <SplashScreen />;
+  let [fontsLoaded] = useFonts({
+    SourceSansPro_400Regular,
+    SourceSansPro_700Bold,
+    SourceSansPro_600SemiBold,
+    SourceSansPro_900Black,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  )
 }
--- src/components/atoms/Logo/index.js
+++ src/components/atoms/Logo/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import { LogoImage } from "./styles";
-import logoImage from "../../../../assets/LogoEmpire.png";
+import React from 'react'
+import { LogoImage } from './styles'
+import logoImage from '../../../../assets/LogoEmpire.png'
 
-export const Logo = () => {
-  return <LogoImage source={logoImage} />;
-};
+const sizes = {
+  small: 28,
+  large: 64,
+}
+
+export const Logo = ({ size }) => {
+  return <LogoImage source={logoImage} size={sizes[size || 'large']} />
+}
